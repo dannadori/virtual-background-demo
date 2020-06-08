2e5b50aa5834fac1288d5ca438c478e3cf59a269
wataru okada <wataru.okada@flect.co.jp>
2020-06-09 07:04:46 +0900

    update

--- build/precache-manifest.f65ac387a812124abbd0b287a851518c.js
+++ build/precache-manifest.f65ac387a812124abbd0b287a851518c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "46d7f33f31cdbfd77deb38d58dc12a42",
+    "revision": "0acc8e82dd2669662dce797bb5801729",
     "url": "/index.html"
   },
   {
-    "revision": "f35232a072d5f62f0926",
-    "url": "/static/js/2.6149d9a9.chunk.js"
+    "revision": "d63fd05819592154471b",
+    "url": "/static/js/2.6591a4c3.chunk.js"
   },
   {
     "revision": "741340985030e9e6bf694412b0484216",
-    "url": "/static/js/2.6149d9a9.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.6591a4c3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "259dba1ee1b2757b3a83",
-    "url": "/static/js/main.288a6efe.chunk.js"
+    "revision": "db678493ebdcdfe8147b",
+    "url": "/static/js/main.e568a90a.chunk.js"
   },
   {
     "revision": "0f7d888817e7a10a65a1",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48e2a88ed61fd4f15b2d0f88608b4c7c.js"
+  "/precache-manifest.f65ac387a812124abbd0b287a851518c.js"
 );
 
 self.addEventListener('message', (event) => {
